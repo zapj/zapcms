c8a78ef773bd101cde4ab9b8dff8d28b6934716d
z <z@zap.cn>
2026-04-20 16:39:43 +0800

    update version

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,8 +8,8 @@
  *
  */
 
-const ZAP_CMS_VERSION = '1.0.1';
-const ZAP_CMS_RELEASE_DATE = '2025-4-22';
+const ZAP_CMS_VERSION = '1.0.2';
+const ZAP_CMS_RELEASE_DATE = '2026-4-20';
 
 if (PHP_VERSION_ID < 70400) {
     exit('ZAP CMS requires PHP 7.4.0+.');
