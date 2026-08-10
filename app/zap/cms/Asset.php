16d97f1c369d609d5731401581ce415762e26932
z <z@zap.cn>
2026-08-10 14:07:22 +0800

    add ace

--- app/zap/cms/Asset.php
+++ app/zap/cms/Asset.php
@@ -125,9 +125,11 @@ CSS, ASSETS_HEAD_TEXT);
                 if (is_dir(app()->basePath('/assets/plugins/ace'))) {
                     register_scripts(base_url('/assets/plugins/ace/ace.js'));
                     register_scripts(base_url('/assets/plugins/ace/ext-modelist.js'));
+                    register_scripts(base_url('/assets/plugins/ace/ext-language_tools.js'));
                 } else {
                     register_scripts('https://cdn.staticfile.org/ace/1.29.0/ace.min.js');
                     register_scripts('https://cdn.staticfile.org/ace/1.29.0/ext-modelist.min.js');
+                    register_scripts('https://cdn.staticfile.org/ace/1.29.0/ext-language_tools.min.js');
                 }
             },
 
